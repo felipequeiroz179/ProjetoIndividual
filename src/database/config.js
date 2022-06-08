5ebe4782b37c0057c7c97a98de849fec4e12c6f3
felipequeiroz179 <felipe.queiroz@sptech.school>
2022-06-08 11:58:50 -0300

    Atualizando Feed e métricas

--- src/database/config.js
+++ src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "root",
-    database: "Garen",
-    password: "Jogoslegais123#",
+    user: "aluno",
+    database: "garen",
+    password: "sptech",
 };
 
 function executar(instrucao) {
